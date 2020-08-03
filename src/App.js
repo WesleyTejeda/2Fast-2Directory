1f874915a62437a3e4a8fa65304790af79be6b46
Wesley PC <wesley.tejeda95@gmail.com>
2020-08-02 23:01:35 -0400

    Restrutured images, JSON and html

--- src/App.js
+++ src/App.js
@@ -6,23 +6,13 @@ import Header from './components/header';
 import Directory from './components/directory';
 
 function App() {
+  console.log(JSON)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Nav />
+    <Header />
+    <Directory />
+      </>
   );
 }
 
