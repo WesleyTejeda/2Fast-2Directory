a37c54babf2c25557636cceb35d81f04e6a8910b
Wesley PC <wesley.tejeda95@gmail.com>
2020-08-03 21:59:59 -0400

    Implemented search bar

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function Header() {
+export default class Header extends Component {
+    state = {
+        query: this.props.state.query
+    }
+      
+    handleInputChange = event => {
+    console.log(event.target);
+    this.setState({
+        [event.target.name]: event.target.value
+    });
+    }
+    
+    
+    handleSubmit = event => {
+    event.preventDefault();
+    };
+
+    render() {
     return (
-        <form className="text-center">
+        <form className="text-center form">
             <label>
                 <span>Search: </span>
-                <input type="text" name="name" />
+                <input type="text" name="query" placeholder="Search any column" value={this.state.query} onChange={this.handleInputChange} />
             </label>
         </form>
     )
+    }
 }
