a33cabe7a6acc47f2d15b0960c707fdc0addd307
Wesley PC <wesley.tejeda95@gmail.com>
2020-08-03 22:00:19 -0400

    Reconfigured props to pass JSON and state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Nav from './components/nav';
 import Header from './components/header';
 import Directory from './components/directory';
+const JSON = require("./JSON/characters.json");
 
-function App() {
-  return (
-    <>
-    <Nav />
-    <Header />
-    <Directory />
-      </>
-  );
+
+class App extends Component {
+  state = {
+      query: "",
+      JSON
+  }
+
+  render() {
+    return (
+      <>
+      <Nav />
+      <Header state={this.state.query}/>
+      <Directory state={this.state.query} JSON={this.state.JSON} />
+        </>
+    );
+  } 
 }
 
 export default App;
